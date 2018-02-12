4ca34f15aa2570863f74a21c3cb85997be382678
Marko Jevtic <jefta1997@gmail.com>
2018-02-12 16:39:29 +0100

    proba
    aa

---
+++ index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Personal shit</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+	<link rel="stylesheet" type="text/css" href="assets/css/main.css">
+</head>
+<header class="bgc-yellow">
+	<!--  -->
+	<div class="logo color-black">
+		 Marko Jevtic
+	</div>
+	<div class="navbar">
+		<ul class="nav">
+  <li class="nav-item">
+    <a class="nav-link active" href="#">Welcome</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">About</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">Contact</a>
+ </li>
+</ul>
+</div>
+
+
+
+</header>
+
+
+<body class="bgc-yellow">
+<div class="container">
+	<div class="text-section">
+<p><?php include 'assets/text/randomtext.html' ?> 
+</p>
+</div>
+<div class="image-section">
+	<img src="assets/img/ja.png">
+</div>
+</div>
+<div class="usluge bgc-white color-black">
+	<div class="prvi"><p><?php include 'assets/text/randomtext.html' ?> </p></div>
+	<div class="drugi"><p><?php include 'assets/text/randomtext.html' ?> </p></div>
+	<div class="treci"><p><?php include 'assets/text/randomtext.html' ?> </p></div>
+</div>
+<div class="footer bgc-black color-white">
+	<div class="copyright">
+		Copy 2018. Marko Jevtic
+	</div>
+</div>
+
+
+
+
+	<script type="text/javascript" src="/assets/js/main.js"></script>
+</body>
+</html>
