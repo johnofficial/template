a74872b5f4b4e8f49d91ec63b279c4691184e183
Marko Jevtic <jefta1997@gmail.com>
2018-02-14 16:28:39 +0100

    [ADD]: Phone site

--- index.php
+++ index.php
@@ -5,12 +5,12 @@
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 	<link rel="stylesheet" type="text/css" href="assets/css/main.css">
 	<style>
-      /* Ensure elements are hidden while ScrollEntrance is loading */
       [data-entrance] { visibility: hidden; }
 </style>
+<meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 <header class="bgc-white">
-	<div class="header-wrap">
+	<div class="header-wrap bgc-white">
 	<div class="logo color-black">
 		<img src="assets/img/logo.png">
 	</div>
